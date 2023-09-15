692eeaa8d46d58dfd0091d41f45e9aaab108d03e
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-09-15 11:40:14 +0300

    imported services

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Home from './Components/Home';
 import About from './Components/About';
-
+import Services from './Components/Services';
 
 function App() {
   return (
     <div>
     <Home/>
     <About/>
+    <Services/>
     </div>
   );
 }
